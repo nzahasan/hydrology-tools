db76c32779461b601f44a105abdb0e66803a8272
nazmul <nzahasan@gmail.com>
2018-06-28 10:15:55 +0600

    minor fix

---
+++ BiasCorr.py
@@ -0,0 +1,38 @@
+import scipy
+import numpy
+
+
+class BiasCorrr(object):
+	"""docstring for BiasCorrr"""
+	def __init__(self,obs_data,sim_data):
+
+		self.fitExecute = False
+
+		pass
+
+
+	def rmse():
+		pass
+	
+	# takes sim val optputs actual val
+	def correct(self,sim_val):
+
+		if self.fitExecute == False: print("ERROR: Execute fit first.")
+
+		pass
+
+	def deltaFit(self):
+		pass
+
+	def scaleFit(self):
+		pass
+
+	def deltaScale(self):
+		pass
+
+	def gammaQuantileFit(self):
+		pass
+
+	def empericalQuantileFit(self):
+		pass
+
